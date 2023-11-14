218c74ae7757b5692daba3e45bcfda3373327255
Komla Adzam <adzamkomla.dev@gmail.com>
2023-11-14 22:04:51 +0000

    Update layout of auth

--- modules/auth/src/lib/@common/layout/layout.component.ts
+++ modules/auth/src/lib/@common/layout/layout.component.ts
@@ -1,16 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-
-import { Observable } from 'rxjs';
-
-export interface Forecast {
-  date: string;
-  temperatureC: number;
-  summary: string;
-  temperatureF: number;
-}
 
 @Component({
   standalone: true,
@@ -18,14 +8,5 @@ export interface Forecast {
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent implements OnInit {
-  weatherForecast$!: Observable<Forecast[]>;
-
-  constructor(private http: HttpClient) { }
-
-  ngOnInit(): void {
-    this.weatherForecast$ = this.http.get<Forecast[]>(
-      'https://gateways.users.phalco.africa/weatherforecast'
-    );
-  }
+export class LayoutComponent {
 }
